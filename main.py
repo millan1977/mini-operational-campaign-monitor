d0382b78ec96d96b4c3853a5189f75942e6fd35e
Rafael Millán Quesada <220942256+millan1977@users.noreply.github.com>
2026-08-31 19:06:25 -0600

    Add elapsed days and OSI calculation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 print("Mini Operational Campaign Monitor")
-Line_Item = {
+line_item = {
     "Advertiser_Name" : "CocaCola",
     "Advertiser_ID" : 100001,
     "Order_Name" : "Fiestas Patrias 2026",
@@ -15,8 +15,19 @@ Line_Item = {
     "Trafficker_ID" : 10001,
     "Status" : "Delivering"
 }
-start_date = datetime.strptime(Line_Item["LineItem_StartDate"],"%d-%m-%Y %H:%M")
-end_date = datetime.strptime(Line_Item["LineItem_EndDate"],"%d-%m-%Y %H:%M")
-dias_totales = (end_date - start_date).days +1
+start_date = datetime.strptime(line_item["LineItem_StartDate"],"%d-%m-%Y %H:%M")
+end_date = datetime.strptime(line_item["LineItem_EndDate"],"%d-%m-%Y %H:%M")
+dias_totales = (end_date - start_date).days + 1 
 print(f"{dias_totales} dias totales")
-print(type(dias_totales))
+today = datetime.now()
+elapsed_days = (today - start_date).days    
+if elapsed_days == 0:
+    osi = None
+else:
+    osi = (line_item["Lifetime_Impressions"] / line_item["Contracted_Impressions"]) / (elapsed_days/dias_totales)
+print(f"Han transcurrido {elapsed_days} dias desde el inicio")
+if osi is None:
+    print(f"No podemos calcular el OSI")
+else:
+    print(f"El OSI de este LineItem es {osi:.2%}")
+    
