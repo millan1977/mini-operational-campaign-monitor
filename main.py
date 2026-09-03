970651bdf965056f39b06fb8186ebc73effe399c
Rafael Millán Quesada <220942256+millan1977@users.noreply.github.com>
2026-09-03 17:48:18 -0600

    Refactor main to use shared metrics module

--- main.py
+++ main.py
@@ -1,33 +1,17 @@
 import json
 from pathlib import Path
 from datetime import datetime
+from metrics import calcular_dias_totales, calcular_dias_transcurridos, calcular_delivery_ratio, calcular_time_ratio, calcular_osi
 
 base_path = Path(__file__).parent
-json_path = base_path / "line_items.json"
+json_file = "line_items.json"
+json_path = base_path / json_file
 with open(json_path, "r") as file:
     dataset = json.load(file)
 
 print("Mini Operational Campaign Monitor")
-today = datetime.now()
 
-def calcular_dias_totales(start_date, end_date):
-    dias_totales = (end_date - start_date).days + 1
-    return dias_totales
-def calcular_dias_transcurridos(today, start_date):
-    elapsed_days = (today - start_date).days
-    return elapsed_days
-def calcular_delivery_ratio(lifetime_impressions, contracted_impressions):
-    delivery_ratio = lifetime_impressions / contracted_impressions
-    return delivery_ratio 
-def calcular_time_ratio(elapsed_days, dias_totales):
-    time_ratio = elapsed_days / dias_totales
-    return time_ratio
-def calcular_osi(delivery_ratio, time_ratio):
-    if time_ratio == 0:
-        osi = None
-    else:
-        osi = delivery_ratio / time_ratio
-    return osi
+today = datetime.now()
 
 for li in dataset:
     start_date = datetime.strptime(li["LineItem_StartDate"], "%d-%m-%Y %H:%M")
